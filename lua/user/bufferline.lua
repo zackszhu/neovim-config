7d96441af991ef13408a142c89855e77fc44de22
Chaojie Zhu <zackspbc@gmail.com>
2023-03-05 21:51:37 -0800

    icon change

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -18,10 +18,10 @@ bufferline.setup({
             icon = '▎', -- this should be omitted if indicator style is not 'icon'
             style = 'icon',
         },
-		buffer_close_icon = "",
+		buffer_close_icon = "󰅚",
 		-- buffer_close_icon = '',
 		modified_icon = "●",
-		close_icon = "",
+		close_icon = "󰅚",
 		-- close_icon = '',
 		left_trunc_marker = "",
 		right_trunc_marker = "",
