3f8bc5a98884fb50e2f5f17fb3140c43e07339e6
Chaojie Zhu <zackspbc@gmail.com>
2023-11-15 21:59:34 -0800

    Format

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -4,14 +4,14 @@ local nvim_tree = require("nvim-tree")
 
 -- OR setup with some options
 nvim_tree.setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,46 +1,45 @@
 local configs = require("nvim-treesitter.configs")
-configs.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = "all",
-  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+configs.setup({
+	-- A list of parser names, or "all"
+	ensure_installed = "all",
+	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
+	-- Automatically install missing parsers when entering buffer
+	auto_install = true,
 
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  rainbow = {
-      enable = true,
-  },
-  indent = {
-      enable = true,
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  },
-}
-
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	rainbow = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+})
 
 -- local just_ok, just = pcall(require, "tree-sitter-just")
 -- if not just_ok then
@@ -48,5 +47,3 @@ configs.setup {
 -- end
 
 -- just.setup({})
-
-
